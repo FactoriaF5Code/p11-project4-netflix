e2408b8b74088b3e1795b8b09b42bf9251c59910
Javi <javi@jsrois.net>
2023-10-19 07:58:51 +0200

    Updated list

--- src/components/MovieList.svelte
+++ src/components/MovieList.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { onMount } from "svelte";
+
     const url =
         "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ca&page=1&sort_by=popularity.desc";
     const options = {
@@ -12,10 +14,12 @@
 
     let movies = [];
 
-    fetch(url, options)
-        .then((response) => response.json())
-        .then((data) => (movies = data.results))
-        .catch((err) => console.error(err));
+    onMount(() =>
+        fetch(url, options)
+            .then((response) => response.json())
+            .then((data) => (movies = data.results))
+            .catch((err) => console.error(err))
+    );
 </script>
 
 <div>
